6c9dd56b1cca3e339f0d9ce8a49723c53771221c
CadarIustin <iustin.cadar@student.nhlstenden.com>
2025-01-16 20:39:03 +0100

    adding more functions to the subtitles controllers and also the module exports
    - adding the getAllSubtitles, updateSubtitles, getSubtitlesById functions
    - adding the module exports of my functions

--- src/controllers/subtitlesController.js
+++ src/controllers/subtitlesController.js
@@ -74,3 +74,71 @@ const getSubtitlesByMediaId = async (req, res) => {
     }
 };
 
+const getSubtitlesById = async (req, res) => {
+    const { subtitlesId } = req.params;
+
+    try {
+        const subtitles = await Subtitles.findByPk(subtitlesId);
+
+        if (!subtitles) {
+            return res.status(404).json({
+                message: "Subtitles not found.",
+            });
+        }
+
+        return res.status(200).json(subtitles);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: "Internal server error",
+        });
+    }
+};
+
+const updateSubtitles = async (req, res) => {
+    const { subtitlesId } = req.params;
+    const updatedData = req.body;
+
+    try {
+        const [updated] = await Subtitles.update(updatedData, {
+            where: { subtitles_id: subtitlesId },
+        });
+
+        if (!updated) {
+            return res.status(404).json({
+                message: "Subtitles not found or no changes made.",
+            });
+        }
+
+        return res.status(200).json({
+            message: "Subtitles updated successfully.",
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: "Internal server error",
+        });
+    }
+};
+
+const getAllSubtitles = async (req, res) => {
+    try {
+        const subtitles = await Subtitles.findAll();
+
+        return res.status(200).json(subtitles);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: "Internal server error",
+        });
+    }
+};
+
+module.exports = {
+    createSubtitles,
+    deleteSubtitles,
+    getSubtitlesByMediaId,
+    getSubtitlesById,
+    updateSubtitles,
+    getAllSubtitles,
+};
