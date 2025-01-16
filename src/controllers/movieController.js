8e13bb28aa319067f25f9d4ae1a30d412b4e0738
CadarIustin <iustin.cadar@student.nhlstenden.com>
2025-01-16 17:14:22 +0100

    Creating the getMovieById and getMovies functions
    - creating the files for the subtitles controllers and routes

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -55,3 +55,44 @@ const deleteMovie = async (req, res) => {
     }
 };
 
+const getMovieById = async (req, res) => {
+    const { movieId } = req.params;
+
+    try {
+        const movie = await Media.findByPk(movieId);
+
+        if (!movie) {
+            return res.status(404).json({
+                message: "Movie not found.",
+            });
+        }
+
+        return res.status(200).json(movie);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: "Internal server error",
+        });
+    }
+};
+
+const getMovies = async (req, res) => {
+    try {
+        const movies = await Media.findAll();
+
+        return res.status(200).json(movies);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: "Internal server error",
+        });
+    }
+};
+
+module.exports = {
+    createMovie,
+    deleteMovie,
+    getMovieById,
+    getMovies,
+};
+
